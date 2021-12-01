230c229805d2757011195cef705fbdd220e5f9c8
srnml <sclee4@up.edu.ph>
2021-12-02 02:54:10 +0800

    Add comments

--- modules/analyzers/lexical_analyzer.py
+++ modules/analyzers/lexical_analyzer.py
@@ -13,19 +13,17 @@ def lexer(code):
     #list that will serve as the symbol table
     symbolTable = []
 
-    lineCounter = 0
     obtw = False        #obtw comment flag
 
     #split code with new line as delimiter
     lines = code.splitlines()
 
     #get the lexemes per line
-    for line in lines:
-        lineCounter += 1
+    for lineCounter in range (len(lines)):
 
         #keep the delimiter when using re.split() : https://stackoverflow.com/questions/2136556/in-python-how-do-i-split-a-string-and-keep-the-separators
         #split line by double quotes as delimiter but keep delimiter
-        tempList = re.split(r'(["].*["])', line)
+        tempList = re.split(r'(["].*["])', lines[lineCounter])
         
         #list that will hold the "words" in current line
         words = []
@@ -36,7 +34,8 @@ def lexer(code):
             #concatenate words and string split result
             else:
                 words += tempList[i].split()
-                
+        
+        #set skip to 0 and currentLine to empty list
         skip, currentLine = 0, []
 
         #access every element in words
@@ -57,16 +56,16 @@ def lexer(code):
                     if i==0:
                         obtw = True
                     #if not, return error to main
-                    else: return([False, lineCounter, "expected end of expression at: OBTW"])
+                    else: return([False, lineCounter+1, "expected end of expression at: OBTW"])
                 #if current word is TLDR
                 elif words[i] == "TLDR":
                     #if no OBTW (not closed) before this TLDR, return error
-                    if obtw == False: return(False, lineCounter, "variable does not exist: TLDR")
+                    if obtw == False: return(False, lineCounter+1, "variable does not exist: TLDR")
                     #if "TLDR" is at the end of the line, update obtw value
                     if i==len(words)-1: 
                         obtw = False
                     #if not, return error to main
-                    else: return(False, lineCounter, "multiple line comment may not appear on the same line as code")
+                    else: return(False, lineCounter+1, "multiple line comment may not appear on the same line as code")
                 break
 
             #for flagged words wth unique first word as keyword
@@ -82,7 +81,7 @@ def lexer(code):
 
             #for flagged words with not unique first word as keyword
             elif words[i] in flaggedWords2:
-                alreadyComplete = True #flag if current lexeme is complete as a keyword
+                alreadyComplete = True #flag if current word is complete as a keyword
                 skip = 0
                 for possibleNextWords in flaggedWords2[words[i]]:
                     for nextWord in possibleNextWords:
@@ -90,20 +89,20 @@ def lexer(code):
                         if (i+skip+1) < len(words) and words[i+skip+1] == nextWord:
                             skip += 1
                             lexeme = lexeme + " " + nextWord
-                        #not the expected next word of flagged word, reset lexeme to current word and reset skip's value to 0
+                        #not the expected next word of flagged word, set alreadyComplete to False, reset lexeme to current word and skip's value to 0
                         else:
                             alreadyComplete = False
                             lexeme = words[i]
                             skip = 0
-                    #if alreadyComplete remained True, all expected next word was found and concatenated
+                    #if alreadyComplete remained True, all expected next words was found and concatenated
                     if alreadyComplete == True:
                         break
 
             #create token for current lexeme
             currentToken = {}
-            withMatch = False
+            withMatch = False #flag if lexeme was matched to a regex
             for regex in regexPatterns:
-                #if lexeme matches current regex, append lexeme and its type (in a dictionary) to currentToken, and append currentToken to currentLine
+                #if lexeme matches current regex, append lexeme and its type (in a dictionary) to currentToken, and append currentToken to currentLine, set withMatch to True
                 if re.match(regex, lexeme):
                     currentToken["lexeme"] = lexeme
                     currentToken["type"] = regexPatterns[regex]
@@ -112,6 +111,6 @@ def lexer(code):
                     break
             #lexeme was not matched to any regex patterns, return error
             if withMatch == False:
-                return([False, lineCounter, "unknown token at: " + lexeme])
+                return([False, lineCounter+1, "unknown token at: " + lexeme])
         symbolTable.append(currentLine) #append currentLine if not empty list
     return symbolTable if obtw==False else [False, 1, "Segmentation Fault"] #return lexemeList if there is no OBTW comment or was closed by a "TLDR", else return error
