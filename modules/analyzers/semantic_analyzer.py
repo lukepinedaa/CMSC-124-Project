fc4c5d2075144d036893b1b1e7b622f530ea479a
redhizon <rmhizon@up.edu.ph>
2022-01-07 04:18:37 +0800

    Create handler for variable redeclaration error

--- modules/analyzers/semantic_analyzer.py
+++ modules/analyzers/semantic_analyzer.py
@@ -6,9 +6,55 @@ isDeclaring = False
 isMultipleLineRequired = False
 isPrinting = False
 printMe = ""
+currInput = "" # input of user from pop-up menu
+inputWindowIsClosed = True
 
 # for the symbol table
-symbol_table = {} # symbol table (list of dictionaries)
+symbol_table = {"IT": "None"} # symbol table (list of dictionaries)
+
+# for getting input of user (when GIMMEH is input)
+def inputBtnClick(i,lexeme,button_pressed):
+    global currInput, code
+    button_pressed.set(1)
+    currInput = i
+    symbol_table[lexeme] = currInput
+    code = code + lexeme + "=" + currInput
+    print(code)
+
+def on_menu_closing(button_pressed):
+    global currInput
+    button_pressed.set(1)
+    currInput = ""
+    
+def getInput(root,lexeme):
+    global currInput
+    menu = Toplevel(root)
+    menu.resizable(False, False)
+    menu.title("GIMMEH")
+
+    # sets the menu at the center
+    x = root.winfo_x()
+    y = root.winfo_y()
+    g = "340x200+"+str(x+500)+"+"+str(y+200)
+    menu.geometry(g)
+    
+    # label
+    Label(menu, text="Enter Input", font=('Helvetica 17 bold')).pack(pady=30)
+
+    # Texbox
+    input_entry = Entry(menu)
+    input_entry.pack()
+
+    # button
+    button_pressed = IntVar()    
+    go = Button(menu, text="Enter", font=('Helvetica',8), height=1, width=7, bg='SystemButtonFace', command=lambda: inputBtnClick(input_entry.get(),lexeme,button_pressed))
+    go.pack(pady=5)
+
+    menu.protocol("WM_DELETE_WINDOW", lambda: on_menu_closing(button_pressed))
+    
+    go.wait_variable(button_pressed)
+
+    menu.destroy()    
 
 # executes the code 
 def execute():
@@ -24,12 +70,21 @@ def printToConsole(s,terminal):
     terminal.insert(END,s+"\n")
     terminal.config(state=DISABLED)
 
+def checkInLine(line, lexeme):
+    for token in line:
+        if token["lexeme"] == lexeme:
+            return True
+    return False
+
 # interprets the code
-def interpret(lexer_result,terminal):
+def interpret(lexer_result,terminal,root):
     global code, isDeclaring, var_iden, var_val
+    isDeclaring = isInputting = isPrinting = False
+    toBePrinted = ""
     line_counter = 0 # line counter
     for line in lexer_result:
         line_counter += 1
+        isDest = True
         if line == []: # if line is empty, we skip it
             continue
 
@@ -45,29 +100,88 @@ def interpret(lexer_result,terminal):
 
             elif type == "Output Keyword":
                 isPrinting = True
-                code = code + "print("                
+                # code = code + "print("
 
             elif type == "Variable Declaration":
                 isDeclaring = True
                 continue 
 
             elif type == "Variable Identifier":
-                if isDeclaring == True:
-                    symbol_table[token["lexeme"]] = None
-                    # if "ITZ" in line (meaning that it an initialized variable)
-                    code = code + token["lexeme"]  
+                if checkInLine(line, "R"):
+                    if isDest:
+                        if token["lexeme"] not in symbol_table:
+                            printToConsole("ERROR: Variable "+token["lexeme"]+" at Line "+str(line_counter)+" does not exist.",terminal) #error "variable does not exist: token["lexeme"]"
+                            return
+                        var_iden = token["lexeme"]
+                        isDest = False
+                    else:
+                        if token["lexeme"] in symbol_table:
+                            symbol_table[var_iden] = symbol_table[token["lexeme"]]
+                            isDest = True
+                        else:
+                            printToConsole("ERROR: Variable "+token["lexeme"]+" at Line "+str(line_counter)+" does not exist.",terminal) #error "variable does not exist: token["lexeme"]"
+                            return
+
+                elif isDeclaring == True:
+                    if checkInLine(line, "ITZ"):
+                        code = code + token["lexeme"]
+                        if isDest == True:
+                            if token["lexeme"] not in symbol_table:
+                                symbol_table[token["lexeme"]] = None
+                                var_iden = token["lexeme"]
+                                isDest = False
+                            else:
+                                printToConsole("ERROR: Redeclaration of variable "+token["lexeme"]+" at Line "+str(line_counter),terminal) #error "variable does not exist: token["lexeme"]"
+                                return
+                        else:
+                            if token["lexeme"] in symbol_table:
+                                print(token["lexeme"], symbol_table)
+                                symbol_table[var_iden] = symbol_table[token["lexeme"]]
+                                isDest = True
+                            else:
+                                printToConsole("ERROR: Variable "+token["lexeme"]+" at Line "+str(line_counter)+" does not exist.",terminal) #error "variable does not exist: token["lexeme"]"
+                                return
+
+                    else:
+                        symbol_table[token["lexeme"]] = None
+                        code = code + token["lexeme"] + "=None"
                 elif isPrinting:
-                    code = code + token["lexeme"] + ")"
-                    printToConsole(symbol_table[token["lexeme"]],terminal)
-                    
+                    if token["lexeme"] not in symbol_table:
+                        printToConsole("ERROR: Cannot implicitly cast nil"+" at Line "+str(line_counter),terminal) #error "Cannot implicitly cast nil"
+                        return
+                    else: toBePrinted = toBePrinted + symbol_table[token["lexeme"]] 
+                    # code = code + token["lexeme"] + ")"
+                    # printToConsole(symbol_table[token["lexeme"]],terminal)
+                elif isInputting: 
+                    getInput(root, token["lexeme"]) 
 
             elif type == "Variable Assignment":
                 code = code + "="
                 
             elif type == "Integer Literal" or type == "Float Literal" or type == "String Literal" or type == "Boolean Literal":
-                code = code + token["lexeme"]
-        code = code + "\n"
-        isDeclaring = isPrinting = False
-    # print(code)
-    execute()
+                if checkInLine(line, "R"):
+                    symbol_table[var_iden] = token["lexeme"]
+                elif isPrinting:
+                    toBePrinted = toBePrinted + token["lexeme"].replace('"', "")
+                    # code = code + token["lexeme"] + ")"
+                    # printToConsole(token["lexeme"].replace('"', ""),terminal)
+                elif isDeclaring:
+                    if (type == "Integer Literal"):
+                        symbol_table[var_iden] = (token["lexeme"])
+                    elif (type == "Float Literal"):
+                        symbol_table[var_iden] = (token["lexeme"])
+                    elif (type == "String Literal"):
+                        symbol_table[var_iden] = token["lexeme"].replace('"', "")
+                    else:
+                        symbol_table[var_iden] = (token["lexeme"])
+                    code = code + token["lexeme"]
             
+            elif type == "Input Keyword":
+                isInputting = True
+        if isPrinting == True:
+            printToConsole(toBePrinted,terminal)
+            toBePrinted = ""
+        code = code + "\n"
+        isDeclaring = isPrinting = isInputting = False
+    # print(code)
+    execute()      
